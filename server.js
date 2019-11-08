93eaedf0204c323059d9f3c4a8a3ca5146efeb81
Harsh Panchal <harshpanchal@Harshs-MBP.fios-router.home>
2019-11-07 23:41:21 -0500

    User authentication

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const Chatkit = require('pusher-chatkit-server')
+const Chatkit = require('@pusher/chatkit-server')
 
 const app = express()
 
 const chatkit = new Chatkit.default({
   instanceLocator: 'v1:us1:84e32cd1-d29d-437e-8c50-45f4b6a0ee33',
-  key: '1321f264-048c-4d2f-91ca-c42156606667:RFe/vjI+9wZ9QRAXUWI8sM+czFwXPcAEKf/3dATT1bU='
+  key: '1321f264-048c-4d2f-91ca-c42156606667:RFe/vjI+9wZ9QRAXUWI8sM+czFwXPcAEKf/3dATT1bU=',
 })
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,18 +15,25 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.post('/users', (req, res) => {
-  const { userName } = req.body;
+  const { username } = req.body;
 
   chatkit.createUser({
-    name: userName,
-    id: userName
+    name: username,
+    id: username
   })
-    .then(() => res.send(201))
+    .then(() => res.sendStatus(201))
     .catch(error => {
-      if(error.error_type === 'services/chatkit/user/user_already_exists') {
-        
+      if (error.error === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.status(error.sendStatus).json(error)
       }
     })
+})
+
+app.post('/authenticate', (req, res) => {
+  const { grant_type } = req.body;
+  res.json(chatkit.authenticate({ grant_type, userId: req.query.user_id }))
 })
 
 const PORT = 3001
--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React, { Component } from 'react'
-import UsernameForm from './components/UsernameForm'
+import React, { Component } from 'react';
+import UsernameForm from './components/UsernameForm';
+import ChatScreen from './chatScreen';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentScreen: 'WhatIsYourUsernameScreen',
+      currentUsername: '',
+    };
+
+    this.onUserNameSubmit = this.onUserNameSubmit.bind(this);
+  }
+  onUserNameSubmit(username) {
+    fetch(`http://localhost:3001/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username}),
+    }).then(response => {
+      this.setState({ currentUsername: username, currentScreen: 'ChatScreen' })
+    }).catch(error => {
+      console.error(error)
+    })
+  }
   render() {
-    return <UsernameForm onSubmit={username => alert(username)} />
+    const { currentScreen, currentUsername } = this.state;
+
+    if(currentScreen === 'WhatIsYourUsernameScreen') {
+      return <UsernameForm onSubmit={this.onUserNameSubmit} />
+    } else if(currentScreen === 'ChatScreen'){
+      return <ChatScreen currentUsername={currentUsername} />
+    }
+    
   }
 }
 
